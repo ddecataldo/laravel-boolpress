3b31c790ea70693e2a5379e1e917bbcb670d2582
ddecataldo <davidedecataldo92@gmail.com>
2022-01-17 20:34:37 +0100

    Vue Js

---
+++ resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ msgGuests }}</h1>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            msgGuests: "Pagina Guests",
+        };
+    },
+};
+</script>
+
+<style>
+</style>
