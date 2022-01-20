c40081fe4ad9a3b67ffdf2e20734c454ef8be357
ddecataldo <davidedecataldo92@gmail.com>
2022-01-20 17:59:01 +0100

    add button in the page

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -7,9 +7,17 @@
     <ul class="list-group">
         @foreach ($posts as $singlePost)
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                <p>{{$singlePost->title}}</p>
-                <p>{{$singlePost->category}}</p>
-                <p>{{$singlePost->author}}</p>
+                <p class="mb-0">{{$singlePost->title}}</p>
+                <p class="mb-0">{{$singlePost->category}}</p>
+                <p class="mb-0">{{$singlePost->author}}</p>
+                <div class="d-flex" style="gap:5px;">
+                    <a href="{{ route('admin.posts.edit', $singlePost->id) }}" class="btn btn-primary"> Modifica </a>
+                    <form action="{{ route('admin.posts.destroy', $singlePost->id) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn btn-danger">Elimina</button>
+                    </form>
+                </div>
             </li>
         @endforeach
     </ul>
