92cb72dea0edec1308c622f62cf522620c641ed7
ddecataldo <davidedecataldo92@gmail.com>
2022-01-20 17:57:31 +0100

    add name input

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -7,27 +7,27 @@
     @csrf
     <div class="form-group">
         <label for="inputTitle">Title</label>
-        <input type="text" class="form-control" id="inputTitle" placeholder="Titolo articolo">
+        <input type="text" class="form-control" name="title" id="inputTitle" placeholder="Titolo articolo">
     </div>
     <div class="form-group">
         <label for="inputDescription">Descrizione</label>
-        <textarea class="form-control" id="inputDescription" rows="3"></textarea>
+        <textarea class="form-control" id="inputDescription" name="content" rows="3"></textarea>
     </div>
     <div class="form-group">
         <label for="inputThumb">Thumb</label>
-        <input type="text" class="form-control" id="inputThumb" placeholder="Url image">
+        <input type="text" class="form-control" id="inputThumb" name="imageUrl" placeholder="Url image">
     </div>
     <div class="form-group">
         <label for="inputAuthor">Author</label>
-        <input type="text" class="form-control" id="inputAuthor" placeholder="Autore articolo">
+        <input type="text" class="form-control" id="inputAuthor" name="author" placeholder="Autore articolo">
     </div>
     <div class="form-group">
         <label for="inputCategory">Cateogoria</label>
-        <input type="text" class="form-control" id="inputCategory" placeholder="Nome categoria">
+        <input type="text" class="form-control" id="inputCategory" name="category" placeholder="Nome categoria">
     </div>
     <div class="form-group">
         <label for="inputPublished">Pubblicato</label>
-        <input type="text" class="form-control" id="inputPublished" placeholder="Pubblicato">
+        <input type="text" class="form-control" id="inputPublished" name="published" placeholder="Pubblicato">
     </div>
 
     <button type="submit" class="btn btn-primary">Pubblica</button>
