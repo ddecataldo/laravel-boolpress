f8ec84a82e8383fb4be22a6bb4d0568077a757a4
ddecataldo <davidedecataldo92@gmail.com>
2022-01-23 17:02:47 +0100

    add js for WYSIWYG Content Editor

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -35,5 +35,11 @@
     <button type="submit" class="btn btn-primary">Modifica</button>
 
 </form>
+
+<script type="text/javascript" src="//js.nicedit.com/nicEdit-latest.js"></script> 
+<script type="text/javascript">
+      bkLib.onDomLoaded(function() { nicEditors.allTextAreas() });
+</script>
+
 </div>
 @endsection
